b2f8ecee39897c83df6885b7e9eddbf8ba4129b8
markforest <toxerius7@gmail.com>
2018-06-04 16:33:09 +0300

    commit 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Window window = new Window();
+            Button button = new Button();
+            window.Content = button;
+            button.Content = "Click Button";
+            window.Title = "Yours First WPF Application";
+            new Application().Run(window);
+        }
+    }
+}
